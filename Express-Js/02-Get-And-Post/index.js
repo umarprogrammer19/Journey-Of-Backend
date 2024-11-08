a28f89d1694444de066ffdda1e4cee04c968bb18
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-08 15:09:03 +0500

    Make User Route

--- Express-Js/02-Get-And-Post/index.js
+++ Express-Js/02-Get-And-Post/index.js
@@ -2,9 +2,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const users = [{
+    id: 1,
+    name: "Umar Farooq",
+}, {
+    id: 2,
+    name: "Anusha Akhter",
+}]
+
 app.get("/", (req, res) => {
-    console.log("Hello World");
+    res.send("Hello World");
 });
+
+app.get("/users", (req, res) => {
+    res.send(users);
+})
 
 app.listen(port, () => {
     console.log(`Server is Running On Port ${port}`);
