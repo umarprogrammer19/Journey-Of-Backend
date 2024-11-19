57d33c87908894eb19dd06241cc29b3c0fc5f4c0
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-19 23:49:13 +0500

    Post Route

--- Express-Js/03-Connection-With-Frontend/Server/index.js
+++ Express-Js/03-Connection-With-Frontend/Server/index.js
@@ -4,8 +4,33 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT;
 
+const users = [{
+    id: 1,
+    name: "Umar Farooq",
+}, {
+    id: 2,
+    name: "Ali Khan",
+}, {
+    id: 3,
+    name: "Ahmed",
+}]
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+    res.status(200).json(users);
+})
+
+app.post("/user", (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400), json({ message: "Name is required" });
+    const user = {
+        id: users.length + 1,
+        name,
+    };
+    users.push(user);
 });
 
 app.listen(port, () => {
