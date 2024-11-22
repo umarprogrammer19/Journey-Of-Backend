b22bf565b7df252175d930f99e71972e5da1ed5c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-22 22:13:48 +0500

    Tailwind Setup

--- Express-Js/04-Database/Server/index.js
+++ Express-Js/04-Database/Server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import "dotenv/config";
 import connectDatabase from "./src/db/index.js";
 import userRoutes from "./src/routes/users.routes.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1", userRoutes);
 
 connectDatabase().then(() => {
