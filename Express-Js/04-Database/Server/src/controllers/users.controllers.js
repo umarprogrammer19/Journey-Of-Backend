70dd6255942430255b036499940311f8c56aeb71
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-22 21:24:19 +0500

    Updated User Successfully

--- Express-Js/04-Database/Server/src/controllers/users.controllers.js
+++ Express-Js/04-Database/Server/src/controllers/users.controllers.js
@@ -28,4 +28,14 @@ const deleteUser = async (req, res) => {
     res.status(202).json({ message: "User Deleted Successfully", users: await Users.find({}) });
 }
 
-export { addUsers, getAllUsers, getSingleUser, deleteUser };
+const updateUser = async (req, res) => {
+    const id = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: "User not found By This Id" });
+    const { name, age } = req.body;
+    if (!name && !age) return res.status(400).json({ message: "name OR age are required" });
+    if (name) await Users.findByIdAndUpdate(id, { name }, { new: true });
+    if (age) await Users.findByIdAndUpdate(id, { age }, { new: true });
+    res.status(202).json({ message: "User Updated Successfully", users: await Users.find({}) });
+}
+
+export { addUsers, getAllUsers, getSingleUser, deleteUser, updateUser };
--- Express-Js/04-Database/Server/src/routes/users.routes.js
+++ Express-Js/04-Database/Server/src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addUsers, deleteUser, getAllUsers, getSingleUser } from "../controllers/users.controllers.js";
+import { addUsers, deleteUser, getAllUsers, getSingleUser, updateUser } from "../controllers/users.controllers.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -13,5 +13,7 @@ router.get("/users", getAllUsers);
 router.get("/users/:id", getSingleUser);
 
 router.delete("/users/:id", deleteUser);
+
+router.put("/users/:id", updateUser);
 
 export default router;
