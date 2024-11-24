6a75c388b4869330459c53b78d5971708108ce2e
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-11-24 19:15:09 +0500

    Successfull Crud

--- Express-Js/04-Database/Client/src/App.jsx
+++ Express-Js/04-Database/Client/src/App.jsx
@@ -34,14 +34,44 @@ export default function App() {
           age: getAge.current.value,
         }),
       });
-      const data = await response.json();
-      console.log(data);
       setError(null);
       getName.current.value = "";
       getAge.current.value = "";
     } catch (error) {
       setError("error To Add User", error.message);
     };
+  }
+
+  const editUser = async (id) => {
+    const newName = prompt("Enter New Name");
+    const newAge = prompt("Enter New Age");
+    if (!newName && !newAge) return alert("Please Enter Name Or Age To Edit");
+    try {
+      await fetch(`http://localhost:3000/api/v1/users/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: newName,
+          age: Number(newAge),
+        }),
+      });
+      setError(null);
+    } catch (error) {
+      console.log("Cannot Edit User", error.message);
+    }
+  }
+
+  const deleteUser = async (id) => {
+    try {
+      await fetch(`http://localhost:3000/api/v1/users/${id}`, {
+        method: "DELETE",
+      });
+      setError(null);
+    } catch (error) {
+      console.log(`Error Deleting User ${error.message}`);
+    }
   }
   return (
     <>
@@ -52,15 +82,15 @@ export default function App() {
         <button type="submit" className="bg-[black] text-white px-2 py-1 rounded">Add User</button>
       </form>
       {error && <p>{error}</p>}
-      {users.length > 0 ? users.map((item, index) => {
+      {users.length > 0 ? users.map((item) => {
         return (
           <div key={item._id} className="flex flex-col mb-5">
             <p>Name: {item.name}</p>
             <p>Age: {item.age}</p>
-            <div>
-              <Link to={`/users/${item._id}`}>View</Link>
-              <button>Delete</button>
-              <button>Edit</button>
+            <div className="flex gap-4">
+              <Link to={`/users/${item._id}`} className="text-blue-600">View</Link>
+              <button className="bg-red-600 text-white px-3 py-1 rounded" onClick={async () => await deleteUser(item._id)}>Delete</button>
+              <button className="bg-green-700 text-white px-3 py-1 rounded" onClick={async () => await editUser(item._id)}>Edit</button>
             </div>
           </div>
         )
