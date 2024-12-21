b0413b54cf7a2c9d2e5d7048cbabc74b38f57001
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-12-21 21:16:26 +0500

    Routing

--- 03-Authentication/02-Full-Auth/Client/src/main.jsx
+++ 03-Authentication/02-Full-Auth/Client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Login from './login.jsx'
+import Signup from './signup.jsx'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Layout />,
+  children: [{
+    path: "",
+    element: <App />
+  }, {
+    path: "/login",
+    element: <Login />
+  }, {
+    path: "/signup",
+    element: <Signup />
+  }]
+}])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
