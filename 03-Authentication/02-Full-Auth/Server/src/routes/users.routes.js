49e4eb77840d070dbd333dc06744d16d136a1579
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-12-21 14:12:22 +0500

    Sign Up Routes

---
+++ 03-Authentication/02-Full-Auth/Server/src/routes/users.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { signUp } from "../controllers/users.controllers.js";
+
+const router = express.Router();
+
+router.post("signup", signUp);
+
+export default router;
