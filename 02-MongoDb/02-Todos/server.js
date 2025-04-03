b70ae1249a9dc1ab590d8a5b30bd751b05533813
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-03 14:09:01 +0500

    Updation

---
+++ 02-MongoDb/02-Todos/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+const todos = [{
+    id: 1,
+    item: "Item1"
+}, {
+    id: 2,
+    item: "Item2"
+}, {
+    id: 3,
+    item: "Item3"
+}];
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.get("/item", (req, res) => {
+    res.status(200).json({
+        message: "Items Get Success",
+        todos
+    })
+});
+
+app.post("/addItems", (req, res) => {
+    const { item } = req.body;
+    todos.push({
+        id: todos.length + 1,
+        item,
+    });
+    res.status(201).json({
+        message: "Items Added Success",
+        todos
+    });
+});
+
+app.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+
+    const index = todos.findIndex((item) => item.id === parseInt(id));
+
+    if (index == -1) return res.status(404).json("Item Not Found");
+    todos.splice(index, 1);
+    res.status(200).json({
+        message: "Item Deleted Success",
+        newItems: todos,
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server is Running on the port", port);
+});
