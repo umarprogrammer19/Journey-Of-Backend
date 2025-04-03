a4043445c289cc03676a849dc6e864e2c53e835a
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-03 17:32:02 +0500

    Mongo Db Edit

--- 02-MongoDb/02-Todos/src/controllers/todos.controllers.js
+++ 02-MongoDb/02-Todos/src/controllers/todos.controllers.js
@@ -53,11 +53,50 @@ export const deleteTodos = async (req, res) => {
             return res.status(400).json({ message: "Invalid ID." });
         }
 
+        const isTodoFound = await todosModels.findById(id);
+
+        if (!isTodoFound) {
+            return res.status(404).json({ message: "Todo Not found." });
+        }
+
         await todosModels.findByIdAndDelete(id);
 
         res.status(201).json({
             message: "Deleted Successfully",
         })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+    };
+};
+
+export const editTodos = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { newTitle } = req.body;
+        if (!id) return res.status(400).json({
+            message: "id not found",
+        });
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid ID." });
+        }
+
+        const isTodoFound = await todosModels.findById(id);
+
+        if (!isTodoFound) {
+            return res.status(404).json({ message: "Todo Not found." });
+        }
+
+        const editedTodo = await todosModels.findByIdAndUpdate(id, { title: newTitle }, { new: true });
+
+        res.status(201).json({
+            message: "Edited Successfully",
+            editedTodo,
+        });
+
     } catch (error) {
         console.log(error.message);
         res.status(500).json({
--- 02-MongoDb/02-Todos/src/routes/todos.routes.js
+++ 02-MongoDb/02-Todos/src/routes/todos.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addTodos, deleteTodos, getTodos } from "../controllers/todos.controllers.js";
+import { addTodos, deleteTodos, editTodos, getTodos } from "../controllers/todos.controllers.js";
 
 const router = express.Router();
 
 router.get("/todos", getTodos);
 router.post("/todos", addTodos);
 router.delete("/todos/:id", deleteTodos);
+router.put("/todos/:id", editTodos);
 
 export default router;
