1d245d668144a640154679c9d1db0890259fad59
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-09 11:23:31 +0500

    Models

--- 02-MongoDb/02-Todos/src/models/todos.models.js
+++ 02-MongoDb/02-Todos/src/models/todos.models.js
@@ -4,6 +4,10 @@ const todoSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
     }
 }, { timestamps: true });
 
---
+++ 02-MongoDb/02-Todos/src/models/users.models.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+export const userModel = mongoose.model("users", userSchema);
+
+export default userModel;
