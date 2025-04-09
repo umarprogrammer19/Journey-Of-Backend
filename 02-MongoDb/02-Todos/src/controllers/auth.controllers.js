8fb2956bdcadd541b681d59c20e1d5361604ebef
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-04-09 16:57:08 +0500

    Improvements

--- 02-MongoDb/02-Todos/src/controllers/auth.controllers.js
+++ 02-MongoDb/02-Todos/src/controllers/auth.controllers.js
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
 
         res.status(200).cookie("access_token", token, {
             expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
-            httpOnly: true,
+            httpOnly: process.env.NODE_ENV === "production",
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         }).json({
